9d74f31a61e5d7ddc39a20d519b7b701d2fb0158
ksanim <sanim1997@gmail.com>
2022-02-04 13:31:44 -0500

    example - using the pump class

--- sampler/scripts/pump_sampler.py
+++ sampler/scripts/pump_sampler.py
@@ -1,27 +1,25 @@
-from gpiozero import PWMLED
+import RPi.GPIO as GPIO
 from time import sleep
 
-class Pump:
-    def __init__(self, pin):              # init function takes in values for the arguments.
-        self.pin = pin                          # putting values in variables
-        
-    def start(self):
-        
-        p = PWMLED(self.pin)
-        p.value = 1.0
-        self.isrunning = 1
-        
-    def stop(self):
-        
-        p = PWMLED(self.pin)
-        p.value = 0
-        self.isrunning = 0
+from pump import Pump
 
-mastersampler = Pump(19)                          # pin 19, 16, 26, 20 are being used 
-sampler_A = Pump(16)
-sampler_B = Pump(26)
-sampler_C = Pump(20)
-        
-if trigger == HIGH:
-    mastersampler.start()
-    if 
+trigger_pin = 40
+mastersampler = Pump(35)              # broadcom pin 19 (35), 16 (36), 26 (37), 20 (38) are being used, the pins in the brackets are physical pins 
+sampler_A = Pump(36)
+sampler_B = Pump(37)
+sampler_C = Pump(38)
+GPIO.setup(trigger_pin,GPIO.IN)
+
+    
+try:  
+    while True:            # this will carry on until you hit CTRL+C  
+        if GPIO.input(40): # if port 25 == 1  
+            mastersampler.start()         # set port/pin value to 1/HIGH/True 
+            print("motor is running") 
+        else:   
+            mastersampler.stop()         # set port/pin value to 0/LOW/False
+            print("motor has stopped")  
+        sleep(0.1)         # wait 0.1 seconds  
+  
+finally:                   # this block will run no matter how the try block exits  
+    GPIO.cleanup()         # clean up after yourself  
